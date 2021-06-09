ef6b440dfc98e90589dd3e0526a339f16f3de30a
0x501D <mail@void.so>
2021-06-09 16:39:14 +0300

    add empty progect

---
+++ app/src/main/java/nemutui/com/github/nemu_droid/MainActivity.kt
@@ -0,0 +1,11 @@
+package nemutui.com.github.nemu_droid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
