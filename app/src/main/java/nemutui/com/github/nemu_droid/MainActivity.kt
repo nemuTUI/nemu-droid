5cbc616af6180f67c5f86e89730fb070a90fc40a
Pavel Balaev <pavel.balaev@3mdeb.com>
2021-06-11 13:47:04 +0300

    disable connect button until all fields is not filled

--- app/src/main/java/nemutui/com/github/nemu_droid/MainActivity.kt
+++ app/src/main/java/nemutui/com/github/nemu_droid/MainActivity.kt
@@ -3,7 +3,10 @@ package nemutui.com.github.nemu_droid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 
 const val EXTRA_NEMU_API_LOCATION = "nemutui.com.github.nemu_droid.EXTRA_NEMU_API_LOCATION"
@@ -14,13 +17,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        api_conn_et = findViewById<EditText>(R.id.nemu_api_location)
+        api_port_et = findViewById<EditText>(R.id.nemu_api_port)
+        api_pass_et = findViewById<EditText>(R.id.nemu_api_password)
+
+        api_conn_et.addTextChangedListener(connect_text_watcher)
+        api_port_et.addTextChangedListener(connect_text_watcher)
+        api_pass_et.addTextChangedListener(connect_text_watcher)
     }
 
     fun connectToApi(view: View) {
-        val api_conn_et = findViewById<EditText>(R.id.nemu_api_location)
-        val api_port_et = findViewById<EditText>(R.id.nemu_api_port)
-        val api_pass_et = findViewById<EditText>(R.id.nemu_api_password)
-
         val api_conn = api_conn_et.text.toString()
         val api_port = api_port_et.text.toString()
         val api_pass = api_pass_et.text.toString()
@@ -33,4 +40,22 @@ class MainActivity : AppCompatActivity() {
 
         startActivity(intent)
     }
+
+    private val connect_text_watcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val connect_button = findViewById<Button>(R.id.connect_button)
+            val api_conn = api_conn_et.text.toString()
+            val api_port = api_port_et.text.toString()
+            val api_pass = api_pass_et.text.toString()
+            val state = !api_conn.isEmpty() && !api_port.isEmpty() && !api_pass.isEmpty()
+
+            connect_button.isEnabled = state
+        }
+    }
+
+    private lateinit var api_conn_et : EditText
+    private lateinit var api_port_et : EditText
+    private lateinit var api_pass_et : EditText
 }
