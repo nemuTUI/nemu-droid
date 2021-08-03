bce009bb23e310628c9503df394ebd1d93ed6b6d
0x501D <mail@void.so>
2021-08-03 08:49:33 +0300

    spice: mask assword

--- app/src/main/java/nemutui/com/github/nemu_droid/SettingsFragment.kt
+++ app/src/main/java/nemutui/com/github/nemu_droid/SettingsFragment.kt
@@ -7,14 +7,16 @@ import androidx.preference.EditTextPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-       addPreferencesFromResource(R.xml.pref)
+        addPreferencesFromResource(R.xml.pref)
         val port_pref: EditTextPreference? = findPreference("ssh_port")
         val pass_pref: EditTextPreference? = findPreference("ssh_pass")
-        /* TODO hide password with asterisks */
 
         port_pref?.setOnBindEditTextListener { editText ->
             editText.inputType = InputType.TYPE_CLASS_NUMBER
         }
 
+        pass_pref?.setOnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
     }
 }
